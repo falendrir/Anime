30866daa6b9c877d722567556d3d6398a981fe7a
Franck <40182918+falendrir@users.noreply.github.com>
2025-11-23 00:43:22 +0100

    Add 10 columns of genre
    Split the genre columns to analyze by genre.

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,11 @@
 import pandas as pd
+from PIL.ImageOps import expand
 
 df_anime = pd.read_csv("data/anime_seasonal_20251122.csv")
+
+df_genre = df_anime['genres'].str.split(';', expand=True)
+
+for i in range(df_genre.shape[1]):
+    df_genre.rename(columns={i:'genre_'+str(i)}, inplace = True)
+
+df_anime = pd.concat([df_anime, df_genre], axis=1)
